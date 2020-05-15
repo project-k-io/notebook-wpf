5a320a62d5fea4aac43e0cbec4024bbf554df8d1
Alan Kharebov <ak61us@gmail.com>
2020-05-14 18:45:36 -0700

    Cleanup App logic

--- ProjectApp/App.xaml.cs
+++ ProjectApp/App.xaml.cs
@@ -17,7 +17,6 @@ namespace ProjectK.Notebook
     public partial class App : Application
     {
         private static ILogger _logger;
-        private bool _canSave;
         private readonly MainViewModel _mainModel = new MainViewModel();
         private MainWindow _mainWindow;
 
@@ -34,136 +33,31 @@ namespace ProjectK.Notebook
         protected override async void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            AddLogging();
-
-            // MainModel
-            var registryPath  = XApp.AppName + "\\Output";
-            var subKey = Registry.CurrentUser.CreateSubKey(registryPath);
-            _mainModel.Output.SetValue = (key, value) => subKey.SetValue(key, value);
-            _mainModel.Output.GetValue = (key, value) => subKey.GetValue(key, value);
-            _mainModel.Output.UpdateFilter = () =>  CollectionViewSource.GetDefaultView(_mainModel.Output.Records).Filter = o => _mainModel.Output.Filter(o);
-            _mainModel.Output.ReadSettings();
+            _mainModel.InitLogging();
+            _logger = LogManager.GetLogger<App>();
+            _mainModel.SetOutput();
 
             // MainWindow
-            _mainWindow = new MainWindow();
-            _mainWindow.DataContext = _mainModel;
+            _mainWindow = new MainWindow {DataContext = _mainModel};
 
             // Show MainWindow
-            LoadSettings();
+            _mainModel.LoadSettings(_mainWindow);
             _mainWindow.Show();
+
             // Load Data
             await _mainModel.LoadDataAsync();
             await _mainModel.UpdateTypeListAsync();
-            await StartSavingAsync();
+            await _mainModel.StartSavingAsync();
         }
-
-
-        private void AddLogging()
-        {
-            try
-            {
-                var serviceProvider = new ServiceCollection()
-                    .AddLogging(logging => logging.AddConsole())
-                    .AddLogging(logging => logging.AddDebug())
-                    .AddLogging(logging => logging.AddProvider(new OutputLoggerProvider(_mainModel.Output.LogEvent)))
-                    .Configure<LoggerFilterOptions>(o => o.MinLevel = LogLevel.Debug)
-                    .BuildServiceProvider();
-
-                LogManager.Provider = serviceProvider;
-                _logger = LogManager.GetLogger<App>();
-                _logger.LogDebug("Logger Installed");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-        }
-
 
         protected override async void OnExit(ExitEventArgs e)
         {
             base.OnExit(e);
-            SaveSettings();
-            StopSaving();
+            _mainModel.SaveSettings(_mainWindow);
+            _mainModel.StopSaving();
             await _mainModel.SaveDataAsync();
             _mainWindow.Close();
             Shutdown();
-        }
-
-        public async Task StartSavingAsync()
-        {
-            _canSave = true;
-            while (_canSave)
-            {
-                await _mainModel.SaveDataAsync();
-                await Task.Run(() => Thread.Sleep(5000));
-            }
-        }
-
-        public void StopSaving()
-        {
-            _canSave = false;
-        }
-
-        private void LoadSettings()
-        {
-            // ISSUE: variable of a compiler-generated type
-            var settings = Settings.Default;
-            try
-            {
-                _logger.LogDebug("LoadSettings");
-
-                // window settings
-                _mainWindow.WindowState = settings.MainWindowState;
-                _mainWindow.Top = settings.MainWindowTop;
-                _mainWindow.Left = settings.MainWindowLeft;
-                _mainWindow.Width = settings.MainWindowWidth;
-                _mainWindow.Height = settings.MainWindowHeight;
-                // model settings
-                _mainModel.Layout.NavigatorWidth = settings.LayoutNavigatorWidth;
-                _mainModel.LastListTaskId = settings.LastListTaskId;
-                _mainModel.LastTreeTaskId = settings.LastTreeTaskId;
-                _mainModel.DataFile = settings.RecentFile;
-                _mainModel.MostRecentFiles.Clear();
-
-                if(File.Exists(_mainModel.DataFile))
-                    _mainModel.MostRecentFiles.Add(new FileInfo(_mainModel.DataFile));
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex);
-            }
-        }
-
-        private void SaveSettings()
-        {
-            try
-            {
-                _logger.LogDebug("SaveSettings()");
-
-                _mainModel.PrepareSettings();
-                var settings = Settings.Default;
-
-                // ISSUE: variable of a compiler-generated type
-                if (_mainWindow.WindowState != WindowState.Minimized)
-                {
-                    settings.MainWindowTop = _mainWindow.Top;
-                    settings.MainWindowLeft = _mainWindow.Left;
-                    settings.MainWindowWidth = _mainWindow.Width;
-                    settings.MainWindowHeight = _mainWindow.Height;
-                    settings.LayoutNavigatorWidth = _mainModel.Layout.NavigatorWidth;
-                    settings.RecentFile = _mainModel.DataFile;
-                    settings.LastListTaskId = _mainModel.LastListTaskId;
-                    settings.LastTreeTaskId = _mainModel.LastTreeTaskId;
-                }
-
-                settings.MainWindowState = _mainWindow.WindowState;
-                settings.Save();
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex);
-            }
         }
     }
 }
--- ProjectApp/MainViewModel.cs
+++ ProjectApp/MainViewModel.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Win32;
 using ProjectK.Logging;
 using ProjectK.Notebook.ViewModels;
+using ProjectK.Utils;
 
 namespace ProjectK.Notebook
 {
     class MainViewModel: NotebookViewModel
     {
-        private readonly ILogger _logger = LogManager.GetLogger<MainViewModel>();
+        private ILogger _logger;
+        private bool _canSave;
 
         public CommandBindingCollection CreateCommandBindings()
         {
@@ -29,7 +35,6 @@ namespace ProjectK.Notebook
             };
             return commandBindings;
         }
-
 
         private void UI_FileOpenOldFormat()
         {
@@ -98,6 +103,114 @@ namespace ProjectK.Notebook
                 return ("", false);
 
             return (dialog.FileName, true);
+        }
+
+        public void InitLogging()
+        {
+            try
+            {
+                var serviceProvider = new ServiceCollection()
+                    .AddLogging(logging => logging.AddConsole())
+                    .AddLogging(logging => logging.AddDebug())
+                    .AddLogging(logging => logging.AddProvider(new OutputLoggerProvider(Output.LogEvent)))
+                    .Configure<LoggerFilterOptions>(o => o.MinLevel = LogLevel.Debug)
+                    .BuildServiceProvider();
+
+                LogManager.Provider = serviceProvider;
+                _logger = LogManager.GetLogger<MainViewModel>();
+                _logger.LogDebug("Logger Installed");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        public void SetOutput()
+        {
+            // MainModel
+            var registryPath = XApp.AppName + "\\Output";
+            var subKey = Registry.CurrentUser.CreateSubKey(registryPath);
+            Output.SetValue = (key, value) => subKey.SetValue(key, value);
+            Output.GetValue = (key, value) => subKey.GetValue(key, value);
+            Output.UpdateFilter = () => CollectionViewSource.GetDefaultView(Output.Records).Filter = o => Output.Filter(o);
+            Output.ReadSettings();
+        }
+
+        public void LoadSettings(Window window)
+        {
+            // ISSUE: variable of a compiler-generated type
+            var settings = Settings.Default;
+            try
+            {
+                _logger.LogDebug("LoadSettings");
+
+                // window settings
+                window.WindowState = settings.MainWindowState;
+                window.Top = settings.MainWindowTop;
+                window.Left = settings.MainWindowLeft;
+                window.Width = settings.MainWindowWidth;
+                window.Height = settings.MainWindowHeight;
+
+                // model settings
+                Layout.NavigatorWidth = settings.LayoutNavigatorWidth;
+                LastListTaskId = settings.LastListTaskId;
+                LastTreeTaskId = settings.LastTreeTaskId;
+                DataFile = settings.RecentFile;
+                MostRecentFiles.Clear();
+
+                if (File.Exists(DataFile))
+                    MostRecentFiles.Add(new FileInfo(DataFile));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex);
+            }
+        }
+
+        public void SaveSettings(Window window)
+        {
+            try
+            {
+                _logger.LogDebug("SaveSettings()");
+
+                PrepareSettings();
+                var settings = Settings.Default;
+
+                // ISSUE: variable of a compiler-generated type
+                if (window.WindowState != WindowState.Minimized)
+                {
+                    settings.MainWindowTop = window.Top;
+                    settings.MainWindowLeft = window.Left;
+                    settings.MainWindowWidth = window.Width;
+                    settings.MainWindowHeight = window.Height;
+                    settings.LayoutNavigatorWidth = Layout.NavigatorWidth;
+                    settings.RecentFile = DataFile;
+                    settings.LastListTaskId = LastListTaskId;
+                    settings.LastTreeTaskId = LastTreeTaskId;
+                }
+
+                settings.MainWindowState = window.WindowState;
+                settings.Save();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex);
+            }
+        }
+
+        public async Task StartSavingAsync()
+        {
+            _canSave = true;
+            while (_canSave)
+            {
+                await SaveDataAsync();
+                await Task.Run(() => Thread.Sleep(5000));
+            }
+        }
+
+        public void StopSaving()
+        {
+            _canSave = false;
         }
 
     }
