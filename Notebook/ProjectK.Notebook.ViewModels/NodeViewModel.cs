26bed4e87b3153a1d898006a1873f0395749e459
Alan Kharebov <ak61us@gmail.com>
2020-09-29 14:36:44 -0700

    Moved KeyboardAction into NodeViewModel

--- Notebook/ProjectK.Notebook.ViewModels/NodeViewModel.cs
+++ Notebook/ProjectK.Notebook.ViewModels/NodeViewModel.cs
@@ -7,7 +7,9 @@ using GalaSoft.MvvmLight.Messaging;
 using Microsoft.Extensions.Logging;
 using ProjectK.Logging;
 using ProjectK.Notebook.Domain;
+using ProjectK.Notebook.ViewModels.Enums;
 using ProjectK.Notebook.ViewModels.Extensions;
+using ProjectK.Notebook.ViewModels.Interfaces;
 using ProjectK.Utils;
 
 namespace ProjectK.Notebook.ViewModels
@@ -312,6 +314,148 @@ namespace ProjectK.Notebook.ViewModels
 
 
         #endregion
+
+        public void KeyboardAction( KeyboardKeys keyboardKeys, IActionService service)
+        {
+            var item = this;
+            var state = service.GetState();
+
+            // don't show logging ctl, alt, shift or arrow keys
+            if (keyboardKeys != KeyboardKeys.None && state != KeyboardStates.None)
+                Logger.LogDebug($"KeyboardAction: {keyboardKeys}, {state}");
+
+            switch (keyboardKeys)
+            {
+                case KeyboardKeys.Insert:
+                    NodeViewModel node;
+                    switch (state)
+                    {
+                        case KeyboardStates.IsShiftPressed:
+                            node = item.Parent.AddNew();
+                            node.Created = DateTime.Now;
+                            break;
+                        case KeyboardStates.IsControlPressed:
+                            var lastSubNode = item.Parent.LastSubNode;
+                            node = item.Parent.AddNew();
+
+                            if (lastSubNode != null)
+                            {
+                                node.Name = item.Name;
+                                node.Created = DateTime.Now;
+                            }
+
+                            break;
+                        default:
+                            node = item.AddNew();
+                            break;
+                    }
+
+                    item.IsSelected = true;
+                    service.SelectItem(item);
+                    service.ExpandItem(item);
+                    service.Handled();
+                    Logger.LogDebug($"Added [{node.Name}] to [{item.Name}]");
+                    break;
+                case KeyboardKeys.Delete:
+                    if (service.DeleteMessageBox())
+                        break;
+
+                    var parent = item.Parent;
+                    if (parent == null)
+                        break;
+
+                    var num1 = parent.Nodes.IndexOf(item);
+                    service.Dispatcher(() => parent.Remove(item));
+
+                    var parentNode = num1 > 0 ? parent.Nodes[num1 - 1] : parent;
+                    if (parentNode == null)
+                        break;
+
+                    service.SelectItem(parentNode);
+                    service.Handled();
+                    break;
+
+                case KeyboardKeys.Left:
+                    if (state == KeyboardStates.IsCtrlShiftPressed)
+                    {
+                        var parent1 = item.Parent;
+                        if (parent1 == null)
+                            break;
+                        var parent2 = parent1.Parent;
+                        if (parent2 == null)
+                            break;
+
+                        parent1.Remove(item);
+
+                        var num2 = parent2.Nodes.IndexOf(parent1);
+                        parent2.Insert(num2 + 1, item);
+                        service.SelectItem(item);
+                        service.Handled();
+                    }
+
+                    break;
+                case KeyboardKeys.Right:
+                    if (state == KeyboardStates.IsCtrlShiftPressed)
+                    {
+                        var parent1 = item.Parent;
+                        if (parent1 == null)
+                            break;
+                        var num2 = parent1.Nodes.IndexOf(item);
+                        if (num2 <= 0)
+                            break;
+
+                        var parentNode2 = parent1.Nodes[num2 - 1];
+                        if (parentNode2 == null)
+                            break;
+
+                        parent1.Remove(item);
+                        parentNode2.Add(item);
+                        service.SelectItem(item);
+                        parent1.IsExpanded = true;
+                        item.IsSelected = true;
+                        service.Handled();
+                    }
+
+                    break;
+                case KeyboardKeys.Up:
+                    if (state == KeyboardStates.IsCtrlShiftPressed)
+                    {
+                        var parent1 = item.Parent;
+                        if (parent1 == null)
+                            break;
+                        var num2 = parent1.Nodes.IndexOf(item);
+                        if (num2 <= 0)
+                            break;
+                        parent1.Remove(item);
+                        parent1.Insert(num2 - 1, item);
+                        service.SelectItem(item);
+                        parent1.IsExpanded = true;
+                        item.IsSelected = true;
+                        service.Handled();
+                    }
+
+                    break;
+                case KeyboardKeys.Down:
+                    if (state == KeyboardStates.IsCtrlShiftPressed)
+                    {
+                        var parent1 = item.Parent;
+                        if (parent1 == null)
+                            break;
+                        var num2 = parent1.Nodes.IndexOf(item);
+                        if (num2 >= parent1.Nodes.Count - 1)
+                            break;
+                        parent1.Remove(item);
+                        parent1.Insert(num2 + 1, item);
+                        service.SelectItem(item);
+                        parent1.IsExpanded = true;
+                        item.IsSelected = true;
+                        service.Handled();
+                    }
+
+                    break;
+            }
+        }
+
 
     }
 }
