ca68ed7f644daa296554888ad0674079d58028e8
Alan Kharebov <ak61us@gmail.com>
2020-12-12 12:42:26 -0800

    Added back ItemView

---
+++ Notebook.WPF/ProjectK.Notebook.Views/Items/ItemView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ProjectK.Notebook.Views.Items
+{
+    /// <summary>
+    /// Interaction logic for PropertiesView.xaml
+    /// </summary>
+    public partial class ItemView : UserControl
+    {
+        public ItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
